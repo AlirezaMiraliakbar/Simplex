732af2569efd4860cda412ba4eb22f4521635016
Alireza Miraliakbar <ar.miraliakbar@gmail.com>
2024-04-26 14:54:02 -0500

    phase I

--- rsimplex.jl
+++ rsimplex.jl
@@ -8,7 +8,7 @@ actual LP problem
 author: Alireza Miraliakbar
 =#
 using LinearAlgebra
-function revised_simplex(A, b, c)
+function revised_simplex(A::Matrix, b::Vector, c::Vector, phase::String)
     #=
     Ax=b
     -------------------------------------------
@@ -16,20 +16,34 @@ function revised_simplex(A, b, c)
     b: the RHS vector of the problem
     c: the cost vector 
     =#
+
     m,n = size(A)
-    B_init = Matrix{Float64}(I, m,m)
-    x_opt = 1
-    return x_opt
+    if phase == "phase I"
+        B_init = Matrix{Float64}(I, m,m)
+        x_B = b
+        c_bar = fill(0,(n,1))
+        c_B = fill(1,(n,1))
+        c_bar_nonbasic = - transpose(c_B) * A
+        c_bar_basic = fill(1,(n,1))
+        tableau = fill(0,(m,n+1))
+        tableau[0,1:]
+        while all(x -> x >= 0, c_bar)
+            
+        end
+    end
+    return c_bar
 end
 # test
-x_test = revised_simplex([1 2 3;4 5 5], [2 ;3], [1 1 1])
 
 # b = []
 # # Phase I
-# A_p1 = []
-# c_p1 = []
-# x_opt_p1 = revised_simplex(A_p1, b, c_p1)
+A= [1 2 3 0; -1 2 6 0; 0 4 9 0; 0 0 3 1]
+b = [2 ;3]
+c =[1; 1; 1]
+x_opt_p1 = revised_simplex(A, b, c, "phase I")
 # # Phase II
-# A_p2 = []
+# A= [1 2 3 0; -1 2 6 0; 0 4 9 0; 0 0 3 1]
+# m, n = size(A)
+
 # c_p2 = []
 # x_opt_p2 = revised_simplex(A_p2, b, c_p2)
